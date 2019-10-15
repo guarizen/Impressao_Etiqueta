62034e8c817b3702192e514681e7399217c9da7a
Roberto Guarize <guarizen@hotmail.com>
2019-10-15 20:30:53 -0300

    Impressao_Etiquetas_Release1

---
+++ Impressao_Etiquetas/ListaRecebimento.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Impressao_Etiquetas
+{
+    public partial class ListaRecebimento
+    {
+        [JsonProperty("odata.metadata")]
+        public Uri OdataMetadata { get; set; }
+
+        [JsonProperty("odata.count")]
+        public long OdataCount { get; set; }
+
+        [JsonProperty("value")]
+        public List<Value> Value { get; set; }
+    }
+
+    public partial class Value
+    {
+        [JsonProperty("produto")]
+        public long Produto { get; set; }
+
+        [JsonProperty("cod_produto")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long CodProduto { get; set; }
+
+        [JsonProperty("descricao1")]
+        public string Descricao1 { get; set; }
+
+        [JsonProperty("cor")]
+        public string Cor { get; set; }
+
+        [JsonProperty("estampa")]
+        public string Estampa { get; set; }
+
+        [JsonProperty("tamanho")]
+        public string Tamanho { get; set; }
+
+        [JsonProperty("ean13")]
+        public string Ean13 { get; set; }
+
+        [JsonProperty("quantidade")]
+        public long Quantidade { get; set; }
+    }
+
+    public partial class ListaRecebimento
+    {
+        public static ListaRecebimento FromJson(string json) => JsonConvert.DeserializeObject<ListaRecebimento>(json, Impressao_Etiquetas.Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this ListaRecebimento self) => JsonConvert.SerializeObject(self, Impressao_Etiquetas.Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+
+    internal class ParseStringConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t) => t == typeof(long) || t == typeof(long?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var value = serializer.Deserialize<string>(reader);
+            long l;
+            if (Int64.TryParse(value, out l))
+            {
+                return l;
+            }
+            throw new Exception("Cannot unmarshal type long");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            if (untypedValue == null)
+            {
+                serializer.Serialize(writer, null);
+                return;
+            }
+            var value = (long)untypedValue;
+            serializer.Serialize(writer, value.ToString());
+            return;
+        }
+
+        public static readonly ParseStringConverter Singleton = new ParseStringConverter();
+    }
+}
---
+++ Impressao_Etiquetas/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Drawing.Printing;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace Impressao_Etiquetas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.Write("Codigo Filial: ");
+                string cod_filial = Convert.ToString(Console.ReadLine());
+                Console.Write("Documento: ");
+                string documento = Convert.ToString(Console.ReadLine());
+
+                var enderecoapi = "http://189.113.4.250:888/api/millenium!pillow/movimentacao/lista_recebimento?romaneio=" + $"{documento}" + "&cod_filial=" + $"{cod_filial}" + "&$format=json";
+
+                var requisicaoWeb = WebRequest.CreateHttp($"{enderecoapi}");
+                requisicaoWeb.Method = "GET";
+                requisicaoWeb.Headers.Add("Authorization", $"Basic YWRtaW5pc3RyYXRvcjp2dGFUUFJAMjAxOSoq");
+                requisicaoWeb.UserAgent = "Consulta API - Recebimento Etiquetas";
+                requisicaoWeb.Timeout = 130000;
+
+                Console.WriteLine("Aguardando Retorno do Millennium...");
+                using (var resposta = requisicaoWeb.GetResponse())
+                {
+                    var streamDados = resposta.GetResponseStream();
+                    StreamReader reader = new StreamReader(streamDados);
+                    object objResponse = reader.ReadToEnd();
+                    var statusCodigo = ((System.Net.HttpWebResponse)resposta).StatusCode;
+
+                    ListaRecebimento Receb = JsonConvert.DeserializeObject<ListaRecebimento>(objResponse.ToString());
+
+                    Console.WriteLine("Processando...");
+                    foreach (var reb in Receb.Value)
+                    {
+                        //Lista de API criar foreach()
+                        string s = "^XA" +
+                                   "^CF0,30" +
+                                   "^FO10,40^FDPILLOWTEX IND. COM. TEXTIL - LTDA^FS" +
+                                   "^CFA,30" +
+                                   "^CF0,20" +
+                                   $"^FO40,220^FDCodigo: {reb.CodProduto} ^FS" +
+                                   $"^FO40,250^FDCor: {reb.Cor}^FS" +
+                                   $"^FO40,280^FDEstampa: {reb.Estampa}^FS" +
+                                   $"^FO40,310^FDTam: {reb.Tamanho}^FS" +
+                                   "^CF0,20" +
+                                   $"^FO26,130^FD{reb.Descricao1}^FS" +
+                                   "^CFA,15" +
+                                   "^BY2,1,100^BEN,100,Y,N" +
+                                   $"^FO80,360^BC^FD{reb.Ean13}^FS" +
+                                   "^XZ";
+
+
+                        for(int i = 1; reb.Quantidade >= i; i++)
+                        {
+                            PrintDialog pd = new PrintDialog();
+                            pd.PrinterSettings = new PrinterSettings();
+                            RawPrinterHelper.SendStringToPrinter(pd.PrinterSettings.PrinterName, s);
+                        }
+                        
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
